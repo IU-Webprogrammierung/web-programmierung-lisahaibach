fe45cdd77d7ab2805514317e6c97f73133d3ea74
Lisa Haibach <lhaibach.win-b2021@fh-salzburg.ac.at>
2025-02-24 09:16:32 +0100

    header & footer eigene Datei

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Header & Footer einfügen
+function loadComponent(id, file) {
+    fetch(file)
+    .then(response => response.text())
+    .then(data => document.getElementById(id).innerHTML = data);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent("header", "header.html");
+    loadComponent("footer", "footer.html");
+});
